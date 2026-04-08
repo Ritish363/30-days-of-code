a5cee5efee2557dd5f44591640e81bec171a4562
Ritish Oswal <ritishoswal@gmail.com>
2026-04-08 21:47:17 +0530

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print("Array:", arr)
+
+print("First element:", arr[0])
+
+print("Array + 2:", arr + 2)
+print("Array * 2:", arr * 2)
+
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+print("2D Array:\n", arr2)
+
+print("Mean:", np.mean(arr))
